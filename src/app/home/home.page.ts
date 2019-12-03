398f5c921410955494d188dffc008f18f0839cd1
Mike Hartington <mikehartington@gmail.com>
2019-12-03 07:07:11 -0500

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-
+import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  photo: SafeResourceUrl;
+  constructor(private sanitizer: DomSanitizer) {}
 
-  constructor() {}
+  async takePicture() {
+    const { Camera } = Plugins;
+    const image = await Camera.getPhoto({
+      quality: 100,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera
+    });
 
+    this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(
+      image && image.dataUrl
+    );
+  }
 }
